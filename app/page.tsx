9c3fd8319bc7d80c666b26acd9e5904229703a4d
VanThu-09 <nguyenvanthu16092004@gmail.com>
2026-03-31 21:38:00 +0700

    update photo

--- app/page.tsx
+++ app/page.tsx
@@ -236,7 +236,7 @@ export default function GraduationInvitation() {
               <div className="main-name">NGUYEN VAN THU</div>
 
               <div className="details">
-                <div>SATURDAY, APRIL 10, 2026</div>
+                <div>SATURDAY, APRIL 11, 2026</div>
                 <div>AT 11:00 AM</div>
                 <div>THANG LONG UNIVERSITY</div>
                 <div className="highlight" style={{ marginTop: "10px" }}>RSVP: 0896557295</div>
